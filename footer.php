9af0af0d9a848801213bb460e2d78582e9bf8a39
andrsaltuve1102 <andrsaltuve1102@gmail.com>
2021-08-21 18:23:57 -0300

    Cambios

---
+++ footer.php
@@ -0,0 +1,15 @@
+<footer class="container">
+        <div class="row mt-5 pb-3">
+            <div class="col-12 col-sm-3 text-center text-sm-start">
+                <a href="https://github.com/andrsaltuve1102" target="_blank" title="Github"><i class="fab fa-github"></i></a>
+                <a href="https://www.linkedin.com/in/andr%C3%A9s-eduardo-altuve-p%C3%A9rez-45ab55116/" target="_blank" title="Linkedin"><i
+                        class="fab fa-linkedin-in"></i></a>
+            </div>
+            <div class="col-12 col-sm-3 text-center text-sm-start pt-2 p-sm-0">
+                Sponsor <a href="https://depcsuite.com" target="_blank" title="DEPCSUITE">DePC Suite</a>
+            </div>
+            <div class="col-12 col-sm-3 text-sm-start  pt-2 p-sm-0">
+                <a href="mailto:andrsaltuve1102@gmail.com">andrsaltuve1102@gmail.com</a>
+            </div>
+        </div>
+    </footer>
